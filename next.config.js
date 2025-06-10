6b186827e6649e8233d0647b5287c17a16bc3db8
Tomaz Pristovnik <tomaz.pristovnik@gmail.com>
2025-06-10 15:13:10 +0200

    volume fix 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // No appDir needed for Next.js 14
+  async headers() {
+    return [
+      {
+        source: '/uploads/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
